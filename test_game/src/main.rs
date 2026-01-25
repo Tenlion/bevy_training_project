40caf384fd56a22b72d5327c6cf43e069441756e
Kira Ash Stephenson <SolaasAshenOnslaught@proton.me>
2026-01-24 18:29:42 -0800

    Sound Works
    Going through Step 4 and got sound working for when the enemies hit the window boundaries.

--- test_game/src/main.rs
+++ test_game/src/main.rs
@@ -3,16 +3,18 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::random;
 
-pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SPEED: f32 = 300.0;
 pub const PLAYER_SIZE: f32 = 16.0;
-pub const ENEMY_COUNT: u8 = 4;
+pub const ENEMY_COUNT: u8 = 3;
+pub const ENEMY_SPEED: f32 = 300.0;
+pub const ENEMY_SIZE: f32 = 16.0;
 
 fn main() {
 
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_player, spawn_camera, spawn_enemies).chain())
-        .add_systems(Update, (player_movement, confine_player_movement).chain())
+        .add_systems(Update, (player_movement, enemy_movement, confine_player_movement, update_enemy_direction).chain())
         .run();
 }
 
@@ -20,7 +22,9 @@ fn main() {
 pub struct Player {}
 
 #[derive(Component)]
-pub struct Enemy {}
+pub struct Enemy {
+    pub direction: Vec2
+}
 
 pub fn spawn_player
 (
@@ -73,12 +77,14 @@ pub fn spawn_enemies
 {
     let window: &Window = window_query.single()?;
 
-    for _ in 0..ENEMY_COUNT {
+    for _ in 0..=ENEMY_COUNT {
         let random_x: f32 = random::<f32>() * window.width();
         let random_y: f32 = random::<f32>() * window.height();
 
         commands.spawn((
-            Enemy {},
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize()
+            },
             Sprite::from_image(asset_server.load("sprites/Items_Star.png")),
             Transform::from_xyz(random_x, random_y, 0.0),
             Visibility::Visible
@@ -160,4 +166,71 @@ pub fn confine_player_movement
     }
 
     Ok(())
+}
+
+pub fn enemy_movement
+(
+    mut enemy_query: Query<(&mut Transform, &Enemy)>,
+    time: Res<Time>
+)
+{
+    for (mut transform, enemy) in enemy_query.iter_mut() {
+        let direction: Vec3 = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0);
+        transform.translation += direction * ENEMY_SPEED * time.delta_secs();
+    }
+}
+
+pub fn update_enemy_direction
+(
+    mut commands: Commands,
+    mut enemy_query: Query<(&Transform, &mut Enemy)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>
+)
+    -> Result<()>
+{
+    let window: &Window = window_query.single()?;
+
+    let half_enemy_size: f32 = ENEMY_SIZE / 2.0;
+    let x_min: f32 = half_enemy_size;
+    let x_max: f32 = window.width() - half_enemy_size;
+    let y_min: f32 = half_enemy_size;
+    let y_max: f32 = window.height() - half_enemy_size;
+
+    for (transform, mut enemy) in enemy_query.iter_mut() {
+
+        let mut direction_changed: bool = false;
+        let translation: Vec3 = transform.translation;
+
+        // Flipping the X direction if a horizontal boundary was hit.
+        if translation.x < x_min || translation.x > x_max {
+            enemy.direction.x *= -1.0;
+            direction_changed = true;
+        }
+
+        // Flipping the Y direction if a vertical boundary was hit.
+        if translation.y < y_min || translation.y > y_max {
+            enemy.direction.y *= -1.0;
+            direction_changed = true;
+        }
+
+        if direction_changed {
+            emit_sound(&mut commands,&asset_server,"audio/wall_hit.wav");
+        }
+    }
+
+    Ok(())
+}
+
+
+// Gabe's dope function for sound.
+pub fn emit_sound(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    path: &'static str
+) {
+    commands.spawn((
+        AudioPlayer::new(asset_server.load(path)),
+        PlaybackSettings::DESPAWN)
+    );
 }
